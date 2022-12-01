64941801e014d885db973f8b3aebc809558b9bc1
terentiy <almazius11@gmail.com>
2022-12-01 18:24:41 +0300

    copy file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,79 @@
-function App(fuckgg = 'pizdec') {
-  const [test, setTest] = useState(fuckgg)
+import React, { useEffect, useState } from 'react'
+import TodoList from './Todo/TodoList'
+import Context from './context'
+import Loader from './Loader'
+import Modal from './Modal/Modal.js'
 
-  return <div>Hello world!</div>
+// const AddTodo = React.lazy(() => import('./Todo/AddTodo'))
+const AddTodo = React.lazy(
+  () =>
+    new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(import('./Todo/AddTodo'))
+      }, 2000)
+    })
+)
+
+function App() {
+  const [todos, setTodos] = useState([])
+  const [loading, setLoading] = React.useState(true)
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then((response) => response.json())
+      .then((api) => {
+        setTimeout(() => {
+          setTodos(api)
+          setLoading(false)
+        }, 1000)
+      })
+  }, [])
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+    )
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id))
+  }
+
+  function dobavit(text) {
+    setTodos(
+      todos.concat([
+        {
+          id: Date.now(),
+          completed: false,
+          title: text,
+        },
+      ])
+    )
+  }
+
+  return (
+    <Context.Provider value={{ removeTodo }}>
+      <div className="wrapper">
+        <h1>React Tutorial</h1>
+        <Modal />
+        <React.Suspense fallback={<Loader />}>
+          <AddTodo dobavit={dobavit} />
+        </React.Suspense>
+
+        {loading && <Loader />}
+        {todos.length ? (
+          <TodoList todos={todos} onToggle={toggleTodo} />
+        ) : loading ? null : (
+          <p>No todos!</p>
+        )}
+      </div>
+    </Context.Provider>
+  )
 }
 
 export default App
