8f41e6e46ad33015077100dd2bf6bb337cd97962
Almaz <almazius11@gmail.com>
2022-12-08 16:38:26 +0300

    create todolist for component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import Todo from './components/Todo'
 
 function App() {
-  return <div>App</div>
+  return (
+    <div>
+      <Todo />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Todo.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+
+const styles = {
+  ul: {
+    listStyle: 'none',
+    margin: 0,
+    padding: 0,
+  },
+  li: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '.5rem 1rem',
+    border: '1px solid #ccc',
+    borderRadius: '4px',
+    marginBottom: '.5rem',
+  },
+  input: {
+    marginRight: '1rem',
+  },
+}
+
+function Todo() {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      completed: false,
+      title: 'Привет мир!',
+    },
+    {
+      id: 2,
+      completed: false,
+      title: 'Привет мир!!!!!!!!!!',
+    },
+    {
+      id: 3,
+      completed: false,
+      title: 'Привет мир!!!!!!!!!!!!!!!!!!!',
+    },
+  ])
+
+  return (
+    <div className="wrapper">
+      <h1>Заметки</h1>
+      <ul style={styles.ul}>
+        {notes.map((element, index) => {
+          return (
+            <li style={styles.li} key={element.id}>
+              {element.title}
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default Todo
